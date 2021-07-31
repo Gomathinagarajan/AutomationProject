3dc649aa79d4ed296d866b893e93ea0946133256
Gomathin <gomathibe10@gmail.com>
2021-07-31 12:50:32 +0530

    Added a new file

---
+++ src/test/java/com/app/tests/AmazonTest1.java
@@ -0,0 +1,27 @@
+package com.app.tests;
+
+import org.testng.annotations.Test;
+
+import com.app.pages.AmazonObject;
+import com.app.pages.BasePage;
+
+public class AmazonTest {
+
+	BasePage bp;
+	AmazonObject AmazonHomepage;
+	
+	public AmazonTest() {
+		bp = new BasePage();
+		AmazonHomepage = new AmazonObject();
+		
+	}
+	@Test
+	public void  verifyDropDown () {
+		AmazonHomepage.selectDropDownSortBy("index(2)");
+		AmazonHomepage.getProductPrices();
+		for(int i=0;i<20;i++) {
+			
+		}
+		
+	}
+}
